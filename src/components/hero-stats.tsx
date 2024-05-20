37326e7d411b9e7715b1b8be12340bf856d1c0dd
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-05-20 21:48:45 +0200

    feat: implement basic stats bar

--- src/components/hero-stats.tsx
+++ src/components/hero-stats.tsx
@@ -1,20 +1,30 @@
 import coin from "../assets/images/coin.png";
+import health from "../assets/images/health.png";
+import stamina from "../assets/images/stamina.png";
 import { useGame } from "../hooks/use-game";
-import { StatsBar } from "./stats-bar";
 
 export const HeroStats = () => {
     const { coins, hero } = useGame();
 
     return (
-        <div className="hero-stats stack horizontal center">
-            <StatsBar type="health" maximum={hero.maxHealth} current={hero.health}>
-                HP:
-            </StatsBar>
-            <StatsBar type="stamina" maximum={hero.maxStamina} current={hero.stamina}>
-                Stamina:
-            </StatsBar>
-            <div className="text small">{coins.current}</div>
-            <img className="w-xl h-xl" src={coin}></img>
+        <div className="hero-stats stack horizontal center loose">
+            <div className="stack horizontal center">
+                <span className="text small offset-sm">
+                    {hero.health} / {hero.maxHealth}
+                </span>
+                <img className="w-xl h-xl" src={health}></img>
+            </div>
+            <div className="stack horizontal center">
+                <span className="text small offset-sm">
+                    {hero.stamina} / {hero.maxStamina}
+                </span>
+                <img className="w-xl h-xl" src={stamina}></img>
+            </div>
+            <div className="claim-right"></div>
+            <div className="stack horizontal center">
+                <span className="text small offset-sm">{coins.current}</span>
+                <img className="w-xl h-xl" src={coin}></img>
+            </div>
         </div>
     );
 };
