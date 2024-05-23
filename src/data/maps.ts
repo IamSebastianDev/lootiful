b94890cefe52b057783261a6a42f5728218d174d
Sebastian Heinz <sebsatian@iamsebastian.dev>
2024-05-23 18:01:54 +0200

    refactor: create temporary big map

--- src/data/maps.ts
+++ src/data/maps.ts
@@ -14,16 +14,37 @@ export const dungeonMap: MapDef<typeof dungeonTileSet> = {
     height: 15,
     // prettier-ignore
     tiles: [
-        "wall_left_1",      "wall_top_2",       "wall_top_1",       "wall_top_3",       "wall_top_3",       "wall_top_3",       "wall_right_1",
+        "wall_left_1",      "wall_top_2",       "wall_top_1",       "wall_top_3",       "wall_top_2",       "wall_top_3","wall_top_1",       "wall_top_3",       "wall_top_2","wall_top_1",       "wall_top_1",       "wall_top_3",
+        "wall_top_1",       "wall_top_2",       "wall_right_1",
         
-        "wall_left_1",      "floor_1",          "floor_3",          "floor_2",          "floor_2",          "floor_2",          "wall_right_1",
+        "wall_left_1",      "floor_2",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_3",          "floor_3", "floor_3",          "floor_3",          "floor_3", "floor_2",          "floor_3",          "wall_right_1",
+
+        "wall_left_1",      "floor_2",          "floor_2",          "floor_2",          "floor_3",          "floor_1", "floor_1",          "floor_2",          "floor_2", "floor_3",          "floor_3",          "floor_2", "floor_2",          "floor_1",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
+
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
         
-        "wall_left_2",      "floor_2",          "floor_1",          "floor_1",          "floor_1",          "floor_1",          "wall_right_2",
+        "wall_left_1",      "floor_1",          "floor_2",          "floor_2",          "floor_2",          "floor_1", "floor_1",          "floor_1",          "floor_1", "floor_1",          "floor_1",          "floor_2", "floor_2",          "floor_2",          "wall_right_1",
         
-        "wall_left_2",      "floor_2",          "floor_3",          "floor_1",          "floor_1",          "floor_1",          "wall_right_1",
+        "wall_corner_l",    "wall_bottom_1",    "wall_bottom_1",          "wall_bottom_1",    "wall_bottom_1",    "wall_bottom_1", "wall_bottom_1",    "wall_bottom_1",    "wall_bottom_1", "wall_bottom_1",    "wall_bottom_1",    "wall_bottom_1", "wall_bottom_1",    "wall_bottom_1",    "wall_corner_r",
         
-        "wall_left_1",      "floor_3",          "floor_3",          "floor_2",          "floor_3",          "floor_3",          "wall_right_2",
-        
-        "wall_corner_l",    "wall_bottom_1",    "wall_bottom_2",    "wall_bottom_1",    "wall_bottom_1",    "wall_bottom_1",    "wall_corner_r"
+       
     ],
 };
