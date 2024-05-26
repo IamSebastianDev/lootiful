103970521cdfd21727e6e28421e7ffa45c471194
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-05-26 22:50:45 +0200

    feat: add sell all button

--- src/components/stopped-screen.tsx
+++ src/components/stopped-screen.tsx
@@ -5,12 +5,19 @@ import { applyCharismaModifier } from "../functions/apply-charisma-modifier";
 import coin from "../assets/images/coin.png";
 
 export const StoppedScreen: React.FC = () => {
-    const { startDungeonDive, lootStore, hero } = useGame();
+    const { startDungeonDive, lootStore, hero, coins } = useGame();
 
     const price = lootStore.collected.reduce(
         (prev, curr) => prev + applyCharismaModifier(curr.item.value, hero.attributes.Charisma.value),
         0
     );
+
+    const sellAllLoot = () => {
+        lootStore.collected.forEach(({ id, item }) => {
+            coins.addCoins(applyCharismaModifier(item.value, hero.attributes.Charisma.value));
+            lootStore.sellCollected(id);
+        });
+    };
 
     return (
         <div className="loot-display">
@@ -23,6 +30,9 @@ export const StoppedScreen: React.FC = () => {
                 <p className="text tiny legible">Sell your loot and upgrade your attributes!</p>
                 <UiButton href="" onClick={() => startDungeonDive()}>
                     <div className="text small">Back into the Dungeon</div>
+                </UiButton>
+                <UiButton href="" onClick={() => sellAllLoot()}>
+                    <div className="text small">Sell all Loot</div>
                 </UiButton>
             </div>
         </div>
