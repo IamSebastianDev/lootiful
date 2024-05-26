bc6efd95f0d351cf039b34d9bd84a3b7ca112afd
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-05-26 23:30:53 +0200

    feat: add more stats

--- src/hooks/use-stats.tsx
+++ src/hooks/use-stats.tsx
@@ -3,12 +3,20 @@ import { useReducer } from "react";
 export type Stats = {
     coins: number;
     rounds: number;
+    skeletonsKilled: number;
+    vampiresKilled: number;
+    treasuresCollected: number;
+    died: number;
 };
 
 type Action =
     | { type: "incrementCoins"; payload: number }
     | { type: "incrementRounds"; payload: number }
+    | { type: "addKill"; payload: "vampire" | "skeleton" | "player" }
+    | { type: "treasureCollected"; payload?: undefined }
     | { type: "reset"; payload?: undefined };
+
+const initialState = { rounds: 0, coins: 0, vampiresKilled: 0, skeletonsKilled: 0, treasuresCollected: 0, died: 0 };
 
 const reducer = (state: Stats, action: Action): Stats => {
     switch (action.type) {
@@ -17,18 +25,20 @@ const reducer = (state: Stats, action: Action): Stats => {
         case "incrementRounds":
             return { ...state, rounds: state.rounds + action.payload };
         case "reset":
-            return { rounds: 0, coins: 0 };
+            return { ...initialState };
         default:
             throw new Error("Unknown action type");
     }
 };
 
 export const useStats = () => {
-    const [state, dispatch] = useReducer(reducer, { coins: 0, rounds: 0 });
+    const [state, dispatch] = useReducer(reducer, { ...initialState });
 
     const trackCoins = (amount: number) => dispatch({ type: "incrementCoins", payload: amount });
     const trackRound = () => dispatch({ type: "incrementRounds", payload: 1 });
+    const trackDeath = () => dispatch({ type: "addKill", payload: "player" });
+    const trackKill = (type: "skeleton" | "vampire") => dispatch({ type: "addKill", payload: type });
     const reset = () => dispatch({ type: "reset" });
 
-    return { state, trackCoins, trackRound, reset };
+    return { state, trackCoins, trackRound, trackDeath, trackKill, reset };
 };
