eab85e09ee27ceb81bba027dcd9f02332f5c7d2d
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-06-20 22:26:47 +0200

    refactor: add additional heading size

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -1,12 +1,12 @@
 /** @format */
 
-import { PropsWithChildren } from 'react';
-import { useClassnames as cls } from '../../hooks/use-classnames';
-import './heading.scss';
+import { PropsWithChildren } from "react";
+import { useClassnames as cls } from "../../hooks/use-classnames";
+import "./heading.scss";
 
 export type HeadingProps = PropsWithChildren & {
-    size: 'sm' | 'md' | 'lg';
+    size: "xs" | "sm" | "md" | "lg";
 };
-export const Heading = ({ size = 'sm', children }: HeadingProps) => {
-    return <div {...cls('ui-heading', size)}>{children}</div>;
+export const Heading = ({ size = "sm", children }: HeadingProps) => {
+    return <div {...cls("ui-heading", size)}>{children}</div>;
 };
