c967c2c1e0367b79ebf5012e0dae7f6b1a1c688b
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-06-20 22:26:33 +0200

    feat: implement base slider component

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -3,6 +3,7 @@
 import { FC, PropsWithChildren } from "react";
 import { useClassnames as cls } from "../../hooks/use-classnames";
 import "./slider.scss";
+import { toPercent } from "../../core/math/to-percent";
 
 export type InputRangeProps = PropsWithChildren & {
     disabled?: boolean;
@@ -11,25 +12,27 @@ export type InputRangeProps = PropsWithChildren & {
     max: number;
     step?: number;
     reversed?: boolean;
-    labeled?: boolean;
+    readValue?: boolean;
     onChange: (value: number) => void;
 };
 
 export const Slider: FC<InputRangeProps> = ({ children, value, min, max, onChange, ...props }) => {
-    const { reversed = false, disabled = false, step = 1, labeled = true } = props;
+    const { reversed = false, disabled = false, step = 1, readValue = false } = props;
     return (
-        <label {...cls("ui-slider", { reversed })}>
-            {children && <div>{children}</div>}
-            <input
-                type="range"
-                value={value}
-                min={min}
-                max={max}
-                step={step}
-                disabled={disabled}
-                onChange={(e) => !disabled && onChange(Number(e.target.value))}
-            />
-            {labeled && <div>{value}</div>}
-        </label>
+        <>
+            <label {...cls("ui-slider", { reversed })}>
+                {children && <div>{children}</div>}
+                <input
+                    type="range"
+                    value={value}
+                    min={min}
+                    max={max}
+                    step={step}
+                    disabled={disabled}
+                    onChange={(e) => !disabled && onChange(Number(e.target.value))}
+                />
+                {readValue && <span>{Math.floor(toPercent(value, 1))}%</span>}
+            </label>
+        </>
     );
 };
